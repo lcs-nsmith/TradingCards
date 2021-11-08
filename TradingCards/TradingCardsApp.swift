6291f3c27c20fa86d1f250d6a48ff6072cebacbd
Nathan Smith <nsmith@lcs.on.ca>
2021-11-08 11:43:27 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Nate S on 2021-11-08.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
