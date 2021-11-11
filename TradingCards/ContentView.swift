1a8a684f9b3faea022e05d040d5d64f8c70e7d2c
Nathan Smith <nsmith@lcs.on.ca>
2021-11-10 21:40:30 -0500

    template done

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -14,25 +14,108 @@ struct ContentView: View {
                 .rotationEffect(.degrees(90))
                 .edgesIgnoringSafeArea(.all)
             
-            Color.blue
-                .edgesIgnoringSafeArea(.all)
-                .padding(.top,695)
-                .padding(.horizontal,320)
-            
-            VStack {
-                HStack {
-                Image("upperdeckcropped1")
-                    .resizable()
-                    .scaledToFit()
+            ZStack {
+                VStack{
+                    Spacer(minLength: 670)
+                    HStack{
+                        Rectangle()
+                            .fill(Color("Celtics Green"))
+                            .padding(.horizontal, 316)
+                            .edgesIgnoringSafeArea(.bottom)
+                    }
+                }
+                VStack {
+                    Spacer(minLength:1)
+                    HStack {
+                        VStack {
+                            Text("Isaiah Thomas")
+                                .font(.largeTitle)
+                                .fontWeight(.semibold)
+                            
+                            Text("5,9 180lbs")
+                                .font(.title)
+                                .fontWeight(.medium)
+                        }
+                        Image("bostonCeltics")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100.875, height: 82.25)
+                    }
                 }
             }
+            VStack {
+                Spacer(minLength: 5)
+                HStack {
+                    Spacer(minLength: 315)
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(Color.white)
+                    Spacer(minLength: 310)
+                }
+                Spacer(minLength: 110)
+            }
             
+            VStack {
+                Spacer(minLength: 10)
+                HStack {
+                    Spacer(minLength: 470)
+                    Image("IT4")
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                        .clipped()
+                    Spacer(minLength: 130)
+                }
+                Spacer(minLength: 115)
+            }
+            
+            VStack {
+                Spacer(minLength: 10)
+                HStack {
+                    Spacer(minLength: 320)
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(Color("Celtics Green"))
+                    Spacer(minLength: 490)
+                }
+                Spacer(minLength: 115)
+            }
+            HStack {
+                Spacer(minLength: 323)
+                VStack {
+                    Image("upperDeck")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 75, height: 62)
+                        .padding(.top)
+                    
+                    Text("""
+Age: 32
+Position: Point Guard
+Avg. Ppg: 18.1
+Avg. Apg: 4.9
+Avg. GmSc: 13
+Career Pts: 9531
+Career Ast: 2604
+Games Played: 528
+Career High Pts: 52
+All-NBA: 1
+Active Player = True
+""")
+                        .font(.headline)
+                    
+                    Text("Isaiah Thomas is an inspiration to many short hoopers. He consistently out worked his competition, earning respect from teamates and competitors alike. At the height of his career, Isaiah Thomas was a top 3 point guard in the NBA")
+                        .padding(.top, 2)
+                        .padding(.leading,5)
+                    Spacer(minLength: 10)
+                }
+                Spacer (minLength: 500)
+            }
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Group {
             ContentView()
+        }
     }
 }
