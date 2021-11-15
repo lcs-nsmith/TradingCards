88a5df73aaf3f1c5c4c7e431029845e49bb99dbb
Nathan Smith <nsmith@lcs.on.ca>
2021-11-15 16:00:05 -0500

    added a file under "shared" for the list

---
+++ TradingCards/Shared/NavigationListView.swift
@@ -0,0 +1,42 @@
+//
+//  NavigationListView.swift
+//  TradingCards
+//
+//  Created by Nate S on 2021-11-15.
+//
+
+import SwiftUI
+
+struct NavigationListView: View {
+    
+    let imageName: String
+    let playerName: String
+    let teamName: String
+    
+    var body: some View {
+        NavigationLink(destination: AllenIversonView() .navigationBarHidden(true)) {
+            HStack {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width:30,height:40, alignment: .center)
+                    .clipped()
+                    .clipShape(Capsule())
+                
+                VStack (alignment: .leading) {
+                    Text(playerName)
+                        .bold()
+                        .multilineTextAlignment(.leading)
+                    Text(teamName)
+                        .multilineTextAlignment(.leading)
+                }
+            }
+        }
+    }
+}
+
+struct NavigationListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationListView(imageName: "AIFACE", playerName: "Allen Iverson", teamName: "Philadelphia 76ers")
+    }
+}
--- TradingCards/TradingCardsApp.swift
+++ TradingCards/TradingCardsApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct TradingCardsApp: App {
     var body: some Scene {
         WindowGroup {
+            NavigationView {
             TradingCardListView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
